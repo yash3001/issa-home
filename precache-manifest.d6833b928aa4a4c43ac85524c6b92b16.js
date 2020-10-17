6aa6c114540a0253b49ff97483d5c249e778656e
yash3001 <yashchauhan197@gmail.com>
2020-10-17 22:35:20 +0530

    Updates

---
+++ precache-manifest.d6833b928aa4a4c43ac85524c6b92b16.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad35fd5e7e6f7509e7c0779bc44c515b",
+    "url": "/issa-home/index.html"
+  },
+  {
+    "revision": "db8793006fd0ba3c2fcd",
+    "url": "/issa-home/static/css/main.ec368f0a.chunk.css"
+  },
+  {
+    "revision": "6ef23201c6697e27d034",
+    "url": "/issa-home/static/js/2.e347c8ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/issa-home/static/js/2.e347c8ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db8793006fd0ba3c2fcd",
+    "url": "/issa-home/static/js/main.85f1a2bc.chunk.js"
+  },
+  {
+    "revision": "10a9878d4f562c8da6a8",
+    "url": "/issa-home/static/js/runtime-main.8934416d.js"
+  },
+  {
+    "revision": "bf5a11c6c28d40d4adce009b92331340",
+    "url": "/issa-home/static/media/Balachandra DS.bf5a11c6.jpg"
+  },
+  {
+    "revision": "49821683be1d72005750ade4f5eb6e93",
+    "url": "/issa-home/static/media/H G Saathvik.49821683.jpg"
+  },
+  {
+    "revision": "9e4e67c4f35c5838174ff8bd065c81fb",
+    "url": "/issa-home/static/media/Khushi S.9e4e67c4.jpg"
+  },
+  {
+    "revision": "a56586f10e0f21e04400c4214f3d1c03",
+    "url": "/issa-home/static/media/Nimisha Bellur.a56586f1.jpg"
+  },
+  {
+    "revision": "7a05187e997d8cda049b876d037c37f1",
+    "url": "/issa-home/static/media/Ruthik Raj N.7a05187e.jpg"
+  },
+  {
+    "revision": "1fe8c04b272ad34da69dc4e1e582af7f",
+    "url": "/issa-home/static/media/Sambhav K Bhandari.1fe8c04b.jpg"
+  },
+  {
+    "revision": "b0eb062806b1d7700b44a9c67368604a",
+    "url": "/issa-home/static/media/Soumya Sai Nanduri.b0eb0628.jpg"
+  },
+  {
+    "revision": "1e9a3f5c29d6673cffdac8f7cf5a54d2",
+    "url": "/issa-home/static/media/Suraksh N S.1e9a3f5c.jpg"
+  },
+  {
+    "revision": "2b7465f8394d89e9c9ffaa68e5f9159b",
+    "url": "/issa-home/static/media/issa-logo.2b7465f8.png"
+  },
+  {
+    "revision": "91378571921034e696f47974b638d49e",
+    "url": "/issa-home/static/media/laptop.91378571.webp"
+  },
+  {
+    "revision": "c279807e5491d7bb56de52c04fcb0186",
+    "url": "/issa-home/static/media/timer.c279807e.webp"
+  }
+]);
